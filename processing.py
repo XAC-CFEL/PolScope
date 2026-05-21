b3b65a0a5ccecf0250236f72a6a38e9ab82327e0
Moritz <mo@redback.de.com>
2026-05-21 15:27:50 +0200

    cont deb

--- processing.py
+++ processing.py
@@ -42,7 +42,7 @@ def process_detector_chunk(args):
 
         # Offset peak positions back to original sample coordinate space.
         results = pf.results
-        print(results)
+        print(results.results)
         if results is not None and hasattr(results, 'empty') and not results.empty and roi_start != 0:
             print("resuklts not empty, applying ROI offset to positions")
             if "pos" in results.columns:
