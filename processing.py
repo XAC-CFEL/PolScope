d695369c9f6a15700fea99a910c8ff4b57146c26
Moritz <mo@redback.de.com>
2026-05-21 15:37:10 +0200

    added .dataframe()

--- processing.py
+++ processing.py
@@ -39,10 +39,11 @@ def process_detector_chunk(args):
             (raw_peak_roi[1] - roi_start) if raw_peak_roi[1] is not None else None,
         ]
         pf.process(roi=peak_roi)
+        pf.dataframe()
 
         # Offset peak positions back to original sample coordinate space.
         results = pf.results
-        print(pf.data)
+        print(results)
         if results is not None and hasattr(results, 'empty') and not results.empty and roi_start != 0:
             print("resuklts not empty, applying ROI offset to positions")
             if "pos" in results.columns:
